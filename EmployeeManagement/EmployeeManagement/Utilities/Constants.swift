8664fe3b345bb42e10dd91c448b18f54a5fd8805
Monika Jdhav <mj00565663@techmahindra.com>
2020-05-20 12:59:33 +0530

    Created "EmployeeManagement" project with basic MVVM pattern folder structure and give support for iOS 12.0 and above.

---
+++ EmployeeManagement/EmployeeManagement/Utilities/Constants.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  EmployeeInformationDemo
+//
+//  Created by Monika Jadhav on 12/05/20.
+//  Copyright © 2020 MJ00565663. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    static let kCellIdentifier = "EmployeeListCell"
+    static let employeeListTitle = "Employee List"
+    static let createNewEmployeeTitle = "Create New Employee"
+    static let ok = "OK"
+    static let stodyboard = "Main"
+    static let employeeListView = "EmployeeListViewController"
+    static let createEmployeeView = "CreateEmployeeViewController"
+    static let employeeListTableCell = "EmployeeListTableViewCell"
+    static let errorTitle = "Error"
+    static let success = "success"
+    static let cresteEmployeeSuccessMessage = "Employee created successfully!"
+    static let message = "Message"
+    static let alert = "Alert"
+    static let defaultImage = UIImage(named: "default")
+    static let backgroundViewColor = UIColor(named: "backViewColour")
+    static let cellLabelTextColor = UIColor(named: "textColour")
+    static let grayButtonColour = UIColor(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
+    static let greenButtonColour = UIColor(red: 38/255, green: 146/255, blue: 66/255, alpha: 1)
+    static let accessibilityIdentifierForEmployeeListTable = "EmployeeListTable"
+}
